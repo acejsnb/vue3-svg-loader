cb37c4364feb3eb3bd7edcfce206f73c0b9440a6
xiongshuang <xsmails@163.com>
2021-11-17 14:46:39 +0800

    fix template bug

--- src/loader/index.js
+++ src/loader/index.js
@@ -3,10 +3,19 @@ module.exports = async function (svg) {
     const { resourcePath, async } = loaderContext;
     // 根据路径获取文件名字
     const pathArr = resourcePath.includes('\\') ? resourcePath.split('\\') : resourcePath.split('/');
-    let name = pathArr.pop();
+    let name = pathArr.pop(),
+        svgStr = svg;
     if (!name.endsWith('.svg')) name = pathArr.pop();
     const callback = async();
     const reg = /<svg(([\s\S])*?)<\/svg>/ig;
-    if (reg.test(svg)) callback(null, `<template><svg class="v-svg-${name.replace('.svg', '')}" ${RegExp.$1}</svg></template>`);
+    const regDefs = /<defs(([\s\S])*?)<\/defs>/ig;
+    const regStyle = /<style(([\s\S])*?)<\/style>/ig;
+    const regT = / t="(([\s\S])*?)"/ig;
+    const regPid = / p-id="(([\s\S])*?)"/ig;
+    if (svgStr.includes('</defs>')) svgStr = svgStr.replace(regDefs, '');
+    if (svgStr.includes('</style>')) svgStr = svgStr.replace(regStyle, '');
+    if (svgStr.includes(' t="')) svgStr = svgStr.replace(regT, '');
+    if (svgStr.includes(' p-id="')) svgStr = svgStr.replace(regPid, '');
+    if (reg.test(svgStr)) callback(null, `<template><svg class="v-svg-${name.replace('.svg', '')}" ${RegExp.$1}</svg></template>`);
     else callback(null, '<template></template>');
 };
